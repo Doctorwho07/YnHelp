9722344005d7b1c1809ed4c153149d1d20897ec3
TerminaTorr45 <chouaiboukouyate@gmail.com>
2023-04-13 17:58:34 +0200

    login

---
+++ public/views/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="../public/css/login.css">
+
+	<title>Page de connexion</title>
+</head>
+<body>
+	<h2>Connexion</h2>
+	<form action="login.php" method="POST">
+		<label for="username">Nom d'utilisateur:</label>
+		<input type="text" id="username" name="username"><br><br>
+		<label for="password">Mot de passe:</label>
+		<input type="password" id="password" name="password"><br><br>
+		<input type="submit" value="S'inscrire">
+		<input type="submit" value="Se connecter">
+	</form>
+</body>
+</html>

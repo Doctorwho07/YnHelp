c810fd6abe034f59b08a8aeca6cc442e86d742af
Doctorwho07 <alexis.rouches@icloud.com>
2023-04-13 18:28:45 +0200

    homepage header footer

--- public/views/home.php
+++ public/views/home.php
@@ -3,14 +3,47 @@
 <head>
     <meta charset="UTF-8">
     <title>YnHelp</title>
-    <link rel="stylesheet" href="home.css" />
+    <link rel="stylesheet" href="./public/css/home.css" />
 </head>
 <body>
 <?php require './public/templates/header.html'; ?>
 
 <div class="background">
-    hello
+    <h1>YnHelp</h1>
+    <h5>Bienvenue sur notre site dédié aux aides et associations destinées à soutenir les étudiants. Nous sommes conscients que la vie étudiante peut être difficile et remplie de défis, c'est pourquoi nous avons rassemblé ici des informations sur les différentes ressources qui sont à votre disposition. Que vous ayez besoin d'aide financière, de soutien psychologique ou d'orientation professionnelle, nous sommes là pour vous aider à trouver les ressources dont vous avez besoin pour réussir et atteindre vos objectifs académiques et personnels. Notre objectif est de vous offrir les informations les plus à jour et pertinentes sur les aides et associations disponibles, afin que vous puissiez prendre des décisions éclairées et avoir accès à tous les outils dont vous avez besoin pour réussir dans vos études.</h5>
 </div>
-<img src="../assets/background.png" alt="background">
+<img src="./public/assets/background.png" alt="background">
+<section>
+    <div class="equipe">
+        <h1>L'équipe</h1>
+    </div>
+<div class="equipe-container">
+    <div class="member">
+    <img src="/public/assets/alexis.jpeg" alt="alexis">
+    <h3>Alexis ROUCHES</h3>
+    <p>B1 informatique</p>
+    </div>
+    <div class="member">
+    <img src="/public/assets/chouaib.jpg" alt="chouaib">
+    <h3>chouaib</h3>
+    <p>B1 informatique</p>
+    </div>
+    <div class="member">
+    <img src="/public/assets/ozlem.jpg" alt="ozlem">
+    <h3>ozlem</h3>
+    <p>B3 cybersécurité</p>
+    </div>
+    <div class="member">
+    <img src="/public/assets/robin.jpg" alt="robin">
+    <h3>robin</h3>
+    <p>B3 infra</p>
+    </div>
+    <div class="member">
+    <img src="/public/assets/ThomasCHAD.jpeg" alt="ThomasCHAD">
+    <h3>Thomas CHAD</h3>
+    <p>B2 informatique</p>
+    </div>
+</div>
+</section>
 </body>
 </html>

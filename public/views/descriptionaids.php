529fbcad62e399ceda67c03d06e0c4f77dfcbf4b
Thomas Tiraboschi <thomas.t6921@gmail.com>
2023-04-14 22:59:59 +0200

    descriptionaids

--- public/views/descriptionaids.php
+++ public/views/descriptionaids.php
@@ -2,17 +2,16 @@
 <html lang="fr">
   <head>
     <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="../public/css/header.css" />
     <link rel="stylesheet" href="../public/css/descriptionaids.css" />
-    <title>❤️ Association ❤️</title>
+    <title>Association</title>
   </head>
   <body>
     <?php require './public/templates/header.html'; ?>
     <div class="article">
-      <h1>
-        <p><?= $aid->getTitle(); ?></p>
-      </h1>
-      <img src="../public/assets/caf.png" alt="caf.pnj" />
+      <h1><?= $aid->getTitle(); ?></h1>
+      <img src="../public/assets/caf.png" alt="caf" />
       <p><?= $aid->getDescription(); ?></p>
       <p><a href="https://www.google.com/" target="_blank">Source</a></p>
     </div>

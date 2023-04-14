47d1590665dc8abec3f56855bfd91d758e1a1099
Lord-forgos <johan.rousseau39@gmail.com>
2023-04-14 22:22:53 +0200

    fix cards

--- public/views/associations.php
+++ public/views/associations.php
@@ -11,15 +11,15 @@
 
 
 <div style="display: flex; flex-direction: row; margin-top: 200px">
-    <?php foreach ($aids as $aid) {
-        $pictureName = $aid->getPictureName() == "" ? "default-user.jpg" : $aid->getPictureName() ?>
-        <a href="/YnHelp/aids/<?= $aid->getId() ?>" style="margin: 25px">
+    <?php foreach ($associations as $association) {
+        $pictureName = $association->getPictureName() == "" ? "default-user.jpg" : $association->getPictureName() ?>
+        <a href="/YnHelp/aids/<?= $association->getId() ?>" style="margin: 25px">
             <div style="width: 18rem; border: 1px solid black; display: flex; flex-direction: column; padding: 15px">
                 <img src="./public/assets/<?= $pictureName ?>" style="padding: 10px 10px 30px; margin-left: 4em"
-                     alt="Image de l'article <?= $aid->getTitle() ?>">
+                     alt="Image de l'article <?= $association->getTitle() ?>">
                 <div>
-                    <h5><?= $aid->getTitle() ?></h5>
-                    <p style="margin-top: 10px"><?= $aid->getResume() ?></p>
+                    <h5><?= $association->getTitle() ?></h5>
+                    <p style="margin-top: 10px"><?= $association->getResume() ?></p>
                 </div>
             </div>
         </a>

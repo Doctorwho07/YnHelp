ae966301e5ea3e81e0053c30648aa766541b33b4
Thomas Tiraboschi <thomas.t6921@gmail.com>
2023-04-14 12:04:12 +0200

    Maj Bdd

--- public/views/aids.php
+++ public/views/aids.php
@@ -9,46 +9,18 @@
 <body>
     <div class="container">
         <?php
-        $fake = [
-            [
-                "id" => 1,
-                "resume" => "Ceci est le résumé de l'article 1",
-                "description" => "Ceci est la description de l'article 1",
-                "title" => "cecie est le titre de l'article 1",
-                "id_organization_type" => [
-                    "id" => 1,
-                    "label" => "aides"
-                ]
-            ], [
-                "id" => 2,
-                "resume" => "Ceci est le résumé de l'article 2",
-                "description" => "Ceci est la description de l'article 2",
-                "title" => "cecie est le titre de l'article 2",
-                "id_organization_type" => [
-                    "id" => 2,
-                    "label" => "association"
-                ]
-            ], [
-                "id" => 3,
-                "resume" => "Ceci est le résumé de l'article 3",
-                "description" => "Ceci est la description de l'article 3",
-                "title" => "cecie est le titre de l'article 3",
-                "id_organization_type" => [
-                    "id" => 1,
-                    "label" => "aides"
-                ]
-            ]
-        ];
-
-        foreach ($fake as $article) {
-            echo '<div class="card">';
-            echo '<div class="card-title">' . htmlspecialchars($article["title"]) . '</div>';
-            echo '<div class="card-resume">' . htmlspecialchars($article["resume"]) . '</div>';
-            echo '<div class="card-description">' . htmlspecialchars($article["description"]) . '</div>';
-            echo '<div class="card-organization">Type d\'organisation: ' . htmlspecialchars($article["id_organization_type"]["label"]) . '</div>';
-            echo '</div>';
-        }
+            print_r($aids);
+            foreach ($aids as $aid) {
+                ?>
+                <div class="card">
+                    <h3 class="card-title"><?= $aid->getTitle() ?></h3>
+                    <p class="card-resume"><?= $aid->getResume() ?></p>
+                    <p class="card-description"><?= $aid->getDescription() ?></p>
+                    <p class="card-organization_type"><?= $aid->getOrganizationType() ?></p>
+                </div>
+                <?php
+            }
         ?>
     </div>
 </body>
-</html> 
+</html>

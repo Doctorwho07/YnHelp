5a8f69c31ffbe35d9ca87c0ce15f9ca32ca64688
Doctorwho07 <alexis.rouches@icloud.com>
2023-04-14 15:19:36 +0200

    userpage

--- public/views/user.php
+++ public/views/user.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8" />
     <title>YnHelp</title>
     <link rel="stylesheet" href="./public/css/home.css" />
+    <link rel="stylesheet" href="./public/css/user.css" />
     <link rel="stylesheet" href="./public/css/header.css" />
     <link rel="stylesheet" href="./public/css/footer.css" />
   </head>
@@ -11,6 +12,23 @@
     <?php require './public/templates/header.html'; ?>
 
     <div class="background"></div>
+    <div class="container">
+      <div class="window">
+        <div class="userinfo">
+          <div class="abc">
+          <div class="picture"></div>
+          <a href="#">Mot de passe oublié</a>
+          </div>
+          
+          <h3>prenom</h3>
+          <h3>nom</h3>
+          <h3>email</h3>
+          
+        </div>
+        <div class="separator"></div>
+        <div class="articles"></div>
+      </div>
+    </div>
   </body>
   <?php require './public/templates/footer.html'; ?>
 </html>
